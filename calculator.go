00e1d5e3d4ab2c6cb171f1ec5515234c0f37182c
Lesovaya Mary <152966237+macabre3k@users.noreply.github.com>
2024-05-05 22:10:08 +0500

    Update calculator.go

--- calculator.go
+++ calculator.go
@@ -1,102 +1,106 @@
 package main
- 
+
 import (
-    "fmt"
-    "strconv"
-    "strings" 
+ "fmt"
+ "strconv"
+ "strings"
 )
- // Карта для преобразования римских чисел в арабские
+
 var romanToArabic = map[string]int{
-    "I": 1, "II": 2, "III": 3, "IV": 4, "V": 5, "VI": 6, "VII": 7, "VIII": 8, "IX": 9, "X": 10,
+ "I": 1, "II": 2, "III": 3, "IV": 4, "V": 5, "VI": 6, "VII": 7, "VIII": 8, "IX": 9, "X": 10,
 }
- // Карта для преобразования арабских чисел в римские
+
 var arabicToRoman = map[int]string{
-    1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX", 10: "X",
+ 1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX", 10: "X",
 }
- // Функция для преобразования римских чисел в арабские
+
 func romanToArabicConverter(roman string) int {
-    result := 0
-    for i := 0;  i < len(roman); i++ { 
-        current  := romanToArabic[string(roman[i])] //текущий = current
-        next := 0
-        if i+1 < len(roman) {
-            next = romanToArabic[string(roman[i+1])]
-        }
-        if next > current {
-            result += next - current
-            i++
-        } else {
-            result += current
-        }
-    }
-    return result
+ result := 0
+ for i := 0; i < len(roman); i++ {
+  current := romanToArabic[string(roman[i])]
+  next := 0
+  if i+1 < len(roman) {
+   next = romanToArabic[string(roman[i+1])]
+  }
+  if next > current {
+   result += next - current
+   i++
+  } else {
+   result += current
+  }
+ }
+ return result
 }
- // Функция для преобразования арабских чисел в римские
+
 func arabicToRomanConverter(arabic int) string {
-    var roman strings.Builder
-    for arabic >= 10 {
-        roman.WriteString("X")
-        arabic -= 10
-    }
-    if arabic >= 9 {
-        roman.WriteString("IX")
-        arabic -= 9
-    }
-    if arabic >= 5 {
-        roman.WriteString("V")
-        arabic -= 5
-    }
-    if arabic >= 4 {
-        roman.WriteString("IV")
-        arabic -= 4
-    }
-    for arabic >= 1 {
-        roman.WriteString("I")
-        arabic -= 1
-    }
-    return roman.String()
+ var roman strings.Builder
+ for arabic >= 10 {
+  roman.WriteString("X")
+  arabic -= 10
+ }
+ if arabic >= 9 {
+  roman.WriteString("IX")
+  arabic -= 9
+ }
+ if arabic >= 5 {
+  roman.WriteString("V")
+  arabic -= 5
+ }
+ if arabic >= 4 {
+  roman.WriteString("IV")
+  arabic -= 4
+ }
+ for arabic >= 1 {
+  roman.WriteString("I")
+  arabic -= 1
+ }
+ return roman.String()
 }
- func examination(num string) float64 {
-    _, ok := romanToArabic[num]
-    if ok {
-        return float64(romanToArabicConverter(num))
-    } else {
-        i, err := strconv.Atoi(num)
-        if err != nil {
-            panic(err)
-        }
-        return float64(i)
- 
-    }
+
+func examination(num string) (float64, bool) {
+ if _, ok := romanToArabic[num]; ok {
+  return float64(romanToArabicConverter(num)), true
+ } else {
+  if arabic, err := strconv.Atoi(num); err == nil {
+   return float64(arabic), false
+  }
+ }
+ return 0, false
 }
- func main() {
-    var inputNum1, inputNum2 string
-    var result float64
-    var operation string
- 
-    fmt.Print("Введите пример:")
-    fmt.Scanf("%s %s %s", &inputNum1, &operation, &inputNum2)
-    num1 := examination(inputNum1)
-    num2 := examination(inputNum2)
-    
-    switch operation {
-    case "+":
-        result = num1 + num2
-    case "-":
-        result = num1 - num2
-    case "*":
-        result = num1 * num2
-    case "/":
-        if num2 != 0 {
-            result = num1 / num2
-        } else {
-            fmt.Println("Ошибка: деление на ноль невозможно")
-            return
-        }
-    default:
-        fmt.Println("Ошибка: неверная операция")
-        return
-    }
- 
-      fmt.Printf("Результат: %s %s %s = %.2f\n", inputNum1, operation, inputNum2, result)
-}
+
+func main() {
+ var inputNum1, inputNum2 string
+ var result float64
+ var operation string
+
+ fmt.Print("Введите пример: ")
+ fmt.Scanf("%s %s %s", &inputNum1, &operation, &inputNum2)
+ num1, isRoman1 := examination(inputNum1)
+ num2, isRoman2 := examination(inputNum2)
+
+ if isRoman1 != isRoman2 {
+  fmt.Println("Ошибка: числа должны быть либо оба римские, либо оба арабские")
+  return
+ }
+
+ switch operation {
+ case "+":
+  result = num1 + num2
+ case "-":
+  result = num1 - num2
+ case "*":
+  result = num1 * num2
+ case "/":
+  if num2 != 0 {
+   result = num1 / num2
+  } else {
+   fmt.Println("Ошибка: деление на ноль невозможно")
+   return
+  }
+ default:
+  fmt.Println("Ошибка: неверная операция")
+  return
+ }
+
+ fmt.Printf("Результат: %s %s %s = %.2f\n", inputNum1, operation, inputNum2, result)
+}   
